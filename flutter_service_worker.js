8df050b2ec8211dfda3d90541c08da3af6187dfc
YouChingLee <fcrdxesz@hotmail.com>
2023-05-29 13:40:12 +0800

    填充空白區域，讓手勢可偵測

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "27cd0df15add3c72ac2ce2e0a842cb4c",
-"/": "27cd0df15add3c72ac2ce2e0a842cb4c",
-"main.dart.js": "0c0ee0c797e8f11b51144dc33998ef8b",
+"index.html": "6f60aa99ac0575de9a53b8bf3630a478",
+"/": "6f60aa99ac0575de9a53b8bf3630a478",
+"main.dart.js": "bda6e5a143f6b7ce5d6e90ef9e77c53c",
 "manifest.json": "59594c52905de98958558b35cab74999",
 "version.json": "d7d8e902b9f77558e5158445038f8a9d"
 };
