cc23acea32a224c6a3b8c89cd6ff86b9bf9e01ba
YouChingLee <fcrdxesz@hotmail.com>
2023-05-29 12:45:33 +0800

    修正字體隨螢幕顯示大小

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d9d6a9c5d230d66a41d6a8e9e11249ef",
-"/": "d9d6a9c5d230d66a41d6a8e9e11249ef",
-"main.dart.js": "36fe35730db279ace250a665f4b1e8b6",
+"index.html": "75997b4085e441393016e146785b69b5",
+"/": "75997b4085e441393016e146785b69b5",
+"main.dart.js": "67e0621c1ee40197f869a0bc4484e99b",
 "manifest.json": "59594c52905de98958558b35cab74999",
 "version.json": "d7d8e902b9f77558e5158445038f8a9d"
 };
